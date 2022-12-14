965d0e06c3e9fed9f31123fc08ef18a3e730eaa7
layonmartins <layon.fonseca@eldorado.org.br>
2022-12-14 08:56:18 -0300

    Aula 123. Spring Pet Clinic - Enhance Owners with Pets and Contact Info

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -7,7 +8,7 @@ import java.util.Set;
  */
 public class Vet extends Person {
 
-    private Set<Speciality> specialitySet;
+    private Set<Speciality> specialitySet = new HashSet<>();
 
     public Set<Speciality> getSpecialitySet() {
         return specialitySet;
